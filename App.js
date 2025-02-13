9c4edb8764493eca8e19eb79238c3b2e25b222e1
ping-eab <angelb.maglasang@gmail.com>
2025-02-13 11:38:16 +0800

    modified:   App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -11,11 +12,18 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerShown: false, // Hides default headers
+          gestureEnabled: true, // Enables swipe gestures
+          animationEnabled: true, // Enables smooth transitions
+        }}
+      >
+        <Stack.Screen name="Splash" component={gestureHandlerRootHOC(SplashScreen)} />
+        <Stack.Screen name="Login" component={gestureHandlerRootHOC(LoginScreen)} />
+        <Stack.Screen name="Home" component={gestureHandlerRootHOC(HomeScreen)} />
+        <Stack.Screen name="History" component={gestureHandlerRootHOC(HistoryScreen)} />
       </Stack.Navigator>
     </NavigationContainer>
   );
